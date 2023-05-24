af028fa561f48388a01b434b2d7fdef94107c258
Roshan singh yadav <callmersy8031@gmail.com>
2023-05-24 10:27:46 +0530

    this is very basic code of react

---
+++ basic/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const a = 100
+  return (
+    <>
+    <h1>My name is Roshan singh yadav</h1>
+    <h2>I am an engineering student</h2>
+    <h3>{5+5}</h3>
+    <h4>{(a)>20?"big":"small"}</h4>
+    </>
+  );
+}
+
+export default App;
